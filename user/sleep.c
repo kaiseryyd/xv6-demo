56882f7dce12d7bd0594e87a8118ea1f21311e72
yangyiduo <18241548640@613.com>
2022-05-15 16:45:36 +0800

    init codes

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+//
+// Created by 杨翊铎 on 2022/5/15.
+//
+
+#include "user.h"
+
+int main(int argc, char *argv[]) {
+    int n;
+    if (argc != 2) {
+        fprintf(2, "Please enter a number \n");
+        exit(1);
+    } else {
+        n = atoi(argv[1]);
+        sleep(n);
+        exit(0);
+    }
+}
